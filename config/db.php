126d8a8a0e6bf8a19ba52001f7951970a32f4aae
gabrie1 <jg.bo@hotmail.com>
2017-12-18 22:31:32 -0400

    BD

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=dbcontabilidad',
-	'username' => 'odoo',
-    'password' => 'odoo',
+    'dsn' => 'pgsql:host=ec2-54-235-244-185.compute-1.amazonaws.com;dbname=d9cjt6bgdioaca',
+	'username' => 'jirkzpggpxyjov',
+    'password' => 'b1b0155248a33f0a7ff5224b335ee8172443ae848df0d5fce04ac642c72cd180',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
